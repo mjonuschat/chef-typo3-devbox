1a0904ff1a1b34f799f72937567d24fb0037a141
Morton Jonuschat <m.jonuschat@mojocode.de>
2016-05-25 11:30:46 +0200

    [TASK] Update to phpMyAdmin 4.6.1

--- attributes/phpmyadmin.rb
+++ attributes/phpmyadmin.rb
@@ -1,5 +1,5 @@
-default['phpmyadmin']['version'] = '4.6.0'
-default['phpmyadmin']['checksum'] = '53e79a93b095714a641fdf7670c98a9cc74995a7652b386c7d87d914c32cff7f'
+default['phpmyadmin']['version'] = '4.6.1'
+default['phpmyadmin']['checksum'] = 'a3887d916482940533b165da3e34c685397b02ffe33db256cbc8166b0339328c'
 default['phpmyadmin']['mirror'] = 'https://files.phpmyadmin.net/phpMyAdmin/'
 default['phpmyadmin']['server_name'] = 'local.typo3.org'
 default['phpmyadmin']['blowfish_secret'] = 'v]e1^U]f\BR\.t0{e_vlz]mqq{GyZ3~UD;K}+mm}a\fi>b2fVC:,\uK~wf>|VMh3'
