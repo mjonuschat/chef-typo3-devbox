1cffba23545dacfe45fe9a2a71ccd490d645847d
Morton Jonuschat <m.jonuschat@mojocode.de>
2016-05-25 11:29:55 +0200

    [TASK] Update Node.js to 4.4.5

--- attributes/nodejs.rb
+++ attributes/nodejs.rb
@@ -1,2 +1,2 @@
-default['nodejs']['version'] = '4.2.3'
-default['nodejs']['checksum'] = '644d4c0b206ebcb75383fbe42f6025e7253a61992816289359d0f4dcdb6087d7'
+default['nodejs']['version'] = '4.4.5'
+default['nodejs']['checksum'] = 'bd6505d8a350cd83907374ea98730b0ba99b97ec45cee418d453a0154384805a'
