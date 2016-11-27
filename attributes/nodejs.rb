a7e0a2ca98d6ffec4e78442db653adc9befe6185
Morton Jonuschat <m.jonuschat@mojocode.de>
2016-11-27 15:26:49 -0800

    [TASK] Update Node.js to v6.9.1

--- attributes/nodejs.rb
+++ attributes/nodejs.rb
@@ -1,2 +1,2 @@
-default['nodejs']['version'] = '4.4.7'
-default['nodejs']['checksum'] = '5ad10465cc9d837c1fda8db0fd1bdc1a4ce823dd6afbc533ac2127e6a9a64133'
+default['nodejs']['version'] = '6.9.1'
+default['nodejs']['checksum'] = 'a9d9e6308931fa2a2b0cada070516d45b76d752430c31c9198933c78f8d54b17'
