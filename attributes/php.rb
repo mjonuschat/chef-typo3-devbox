3a1b1faf46288f57369ff488bed994865f4955e5
Morton Jonuschat <m.jonuschat@mojocode.de>
2016-11-27 15:27:10 -0800

    [TASK] Install PHP 7.1 by default

--- attributes/php.rb
+++ attributes/php.rb
@@ -1,3 +1,3 @@
-default['php']['versions'] = %w(5.5 5.6 7.0)
+default['php']['versions'] = %w(5.5 5.6 7.0 7.1)
 default['php']['extensions'] = %w(bcmath bz2 cli common curl fpm gd gmp imap intl json ldap mbstring mcrypt mysql opcache pgsql pspell readline recode soap sqlite3 xml xmlrpc xsl zip)
 default['php']['pecl_extensions'] = %w(php-apcu php-apcu-bc php-gettext php-pear php-memcached php-redis php-uuid php-xdebug)
